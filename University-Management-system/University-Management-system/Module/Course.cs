2874fef8dbb3407ea3ba73d81034f2d6b400227c
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-06-30 13:35:28 +0400

    create Instructor class

--- University-Management-system/University-Management-system/Module/Course.cs
+++ University-Management-system/University-Management-system/Module/Course.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace University_Management_system.Module
 {
+    [Index(nameof(courseCode),IsUnique = true)]
    public class Course
     {
-       
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int courseId {  get; set; }//system generate
+        [Required]
+        [MaxLength(10)]
+        public string courseCode { get; set; }//user input
+        [Required,MaxLength(150)]
+        public string courseTitle { get; set; }
+        [Required]
+        [Range(1,6)]
+        public int creditHours { get; set; }
+        [ForeignKey(nameof(Department))]
+        public int departmentId { get; set; }
+        [ForeignKey(nameof(Instructor))]
+        public int ? instructorId { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string semesterOffered { get; set; }
+
+
     }
 }
