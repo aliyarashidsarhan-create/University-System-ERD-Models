dda18ce1727d47da546d8190c5a8cf215656d036
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-06-30 13:42:33 +0400

    create Enrollement and cource  class

--- University-Management-system/University-Management-system/Module/Enrollment.cs
+++ University-Management-system/University-Management-system/Module/Enrollment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,19 @@ namespace University_Management_system.Module
 {
     public class Enrollment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int enrollmentId {  get; set; }//system generated
+        [ForeignKey(nameof(Student))]
+        public int studentId { get; set; }
+        [ForeignKey(nameof (Course))]
+        public int courseId { get; set; }
+        [Required]
+        public DateTime enrollmentDate { get; set; }
+        [MaxLength(2)]
+        public string? finalGrade {  get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string status { get; set; } = "In Progress";//defult
     }
 }
