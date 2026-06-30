a0ae86eeecd43bc79aa31f5878418b891db72144
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-06-30 13:24:34 +0400

    create Department class

--- University-Management-system/University-Management-system/Module/Department.cs
+++ University-Management-system/University-Management-system/Module/Department.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace University_Management_system.Module
 {
-    internal class Department
+    [Index(nameof(departmentName),IsUnique =true)]
+    public class Department
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int departmentId {  get; set; }//system generate
+        [Required]
+        [MaxLength(100)]
+        public string departmentName {  get; set; } //user input
+        [MaxLength(50)]
+        public string? building {  get; set; }
+        [Required]
+        [Range(0,double.MaxValue)]
+        public decimal budget { get; set; }
+        [ForeignKey("headInstructor")]
+        public int ?headInstructorId {  get; set; }//foregn key
     }
 }
